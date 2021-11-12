270a91551a6e5c41e22fb7009253785e6cecc2f8
Lara McDonald <mccilara@gmail.com>
2021-11-12 19:43:04 +0100

    Beautify run.py

--- assignment3/src/run.py
+++ assignment3/src/run.py
@@ -17,12 +17,6 @@ train_data, test_data = tfds.load('genomics_ood', split=['train[:10%]', 'test[:1
 train_data = train_data.apply(input_pipeline.prepare_data)
 test_data = test_data.apply(input_pipeline.prepare_data)
 
-train_dataset_numpy = tfds.as_numpy(train_data)
-
-for i in train_dataset_numpy:
-  print(i)
-  break
-
 # Hyperparameters
 num_epochs = 10
 alpha = 0.1
@@ -31,41 +25,42 @@ alpha = 0.1
 model = GenomicsModel()
 
 # loss function
-categorical_cross_entropy_loss = tf.keras.losses.CategoricalCrossentropy()
+cat_cross_entropy = tf.keras.losses.CategoricalCrossentropy()
 
-# optimizer
-optimizer = tf.keras.optimizers.SGD(alpha)
+# stochastic gradient descent optimizer
+sgd_optimizer = tf.keras.optimizers.SGD(alpha)
 
 # Initialize lists for later visualization.
 train_losses = []
-
 test_losses = []
 test_accuracies = []
 
-#testing once before we begin
-test_loss, test_accuracy = training_loop.test(model, test_data, categorical_cross_entropy_loss)
+# testing once before we begin
+test_loss, test_accuracy = training_loop.test(model, test_data, cat_cross_entropy)
 test_losses.append(test_loss)
 test_accuracies.append(test_accuracy)
 
-#check how model performs on train data once before we begin
-train_loss, _ = training_loop.test(model, test_data, categorical_cross_entropy_loss)
+# check how model performs on train data once before we begin
+train_loss, _ = training_loop.test(model, test_data, cat_cross_entropy)
 train_losses.append(train_loss)
 
 # We train for num_epochs epochs.
 for epoch in range(num_epochs):
+
+    # print out starting accuracy
     print(f'Epoch: {str(epoch)} starting with accuracy {test_accuracies[-1]}')
 
-    #training (and checking in with training)
+    # training (and checking in with training)
     epoch_loss_agg = []
     for input,target in train_data:
-        train_loss = training_loop.train_step(model, input, target, categorical_cross_entropy_loss, optimizer)
+        train_loss = training_loop.train_step(model, input, target, cat_cross_entropy, sgd_optimizer)
         epoch_loss_agg.append(train_loss)
     
-    #track training loss
+    # track training loss
     train_losses.append(tf.reduce_mean(epoch_loss_agg))
 
-    #testing, so we can track accuracy and test loss
-    test_loss, test_accuracy = training_loop.test(model, test_data, categorical_cross_entropy_loss)
+    # testing, so we can track accuracy and test loss
+    test_loss, test_accuracy = training_loop.test(model, test_data, cat_cross_entropy)
     test_losses.append(test_loss)
     test_accuracies.append(test_accuracy)
 
