1fded700919e691a286dcdc0211f1d06c2a66372
thonor111 <t.nortmann@outlook.de>
2021-11-27 23:49:20 +0100

    playing with the model parameters, scratching 90%

--- assignment5/model.py
+++ assignment5/model.py
@@ -20,9 +20,15 @@ class Model(keras.Model):
             padding="same", strides=(1, 1),
             activation="relu"
             )
+        self.conv4 = keras.layers.Conv2D(
+            filters=64, kernel_size=(3, 3),
+            padding="same", strides=(1, 1),
+            activation="relu"
+        )
 
         self.max_pool_1 = keras.layers.MaxPool2D()
         self.max_pool_2 = keras.layers.MaxPool2D()
+        self.max_pool_3 = keras.layers.MaxPool2D()
         self.global_pool = keras.layers.GlobalAvgPool2D()
 
         self.out = keras.layers.Dense(10, activation = "softmax")
@@ -34,6 +40,8 @@ class Model(keras.Model):
         x = self.conv2(x)
         x = self.max_pool_2(x)
         x = self.conv3(x)
+        x = self.max_pool_3(x)
+        x = self.conv4(x)
         x = self.global_pool(x)
         x = self.out(x)
         return x
